f54b96f61f681e8411f39c9f4025d8be0e8c3aa8
Naresh <Naresh@DESKTOP-NP2KJ5L>
2018-01-06 20:11:15 +0530

    first commit today

---
+++ git/src/git/Demo.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Demo{
+	public static void main(String[] args) {
+		System.out.println("program is running......");
+		for (int i = 0; i <25; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+}
